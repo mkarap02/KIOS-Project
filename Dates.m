2c8da8dff7354a7b1c121b59b6a83812deb33e7d
Marianna Karapitta <87762051+mkarap02@users.noreply.github.com>
2022-07-08 11:25:57 +0300

    Add files via upload

---
+++ Dates.m
@@ -0,0 +1,15 @@
+function [tab] = Dates(date1,date2)
+
+    infmt='dd/MM/yyyy';
+    datetime.setDefaultFormats('defaultdate','dd/MM/yyyy')
+    
+    tab = readtable('CasesData.csv');
+    tab = sortrows(tab,'FirstSampling','ascend');
+
+    toDelete = tab.FirstSampling<date1;
+    tab(toDelete,:) = [];
+   
+    toDelete = tab.FirstSampling>date2;
+    tab(toDelete,:) = [];
+end
+
