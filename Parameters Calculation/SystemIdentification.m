799b743ecd349d262439775200a0afe2fef35cde
Marianna Karapitta <87762051+mkarap02@users.noreply.github.com>
2022-10-18 13:34:20 +0300

    Add files via upload

---
+++ Parameters Calculation/SystemIdentification.m
@@ -0,0 +1,38 @@
+function [C,x,C_vec] = SystemIdentification(ClearedTrajectories,ClearedVaccinations,count,param,param1,param2,param3,gamma_i,gamma_a,gamma_d,gamma_h,ksi_i,ksi_d,mu_a,mu_h)
+
+    %--------MODEL--------
+    
+    %x = (s,i,d,a,r,e,v,h)
+    
+    %Initial conditions
+    x(1,1) = ClearedTrajectories.Susceptible(1);            %S : Susceptible
+    x(2,1) = ClearedTrajectories.Infected(1);               %I : Infected detected
+    x(3,1) = ClearedTrajectories.VaccinatedInfected(1);     %D : Vaccinated infected Detected
+    x(4,1) = ClearedTrajectories.Hospitalized(1);           %A : Acutely Symptomatic
+    x(5,1) = ClearedTrajectories.Recovered(1);              %R : Recovered
+    x(6,1) = ClearedTrajectories.Extinct(1);                %E : Extinct
+    x(7,1) = ClearedTrajectories.VaccinatedSusceptible(1);  %V : Vaccinated Susceptible
+    x(8,1) = ClearedTrajectories.VaccinatedHospitalized(1); %H : Vaccinated acutely symptomatic
+    
+    Rho = 5.08;                         %Basic reproduction number for D-variant
+    
+    dt = 1;                             %time increments  
+    T_days = count;                     %Number of days
+    T = T_days/dt;
+
+    %zeta = 0.1;                         %Initialisation of z
+
+    %Epidem Function calling
+    for k=2:T
+        vaccperday = ClearedVaccinations.NewPeopleVaccinatedPerDay(k-1);
+        %vaccperday
+        x(:,k) = Dynamics(dt, x(:,k-1), param, param1, param2, param3, vaccperday, gamma_i, gamma_a, gamma_d, gamma_h, ksi_i, ksi_d, mu_a, mu_h);
+        x(7,k) = ClearedTrajectories.VaccinatedSusceptible(k);          
+    end
+
+    %check if its 1
+
+    %Function to calculate Cost
+    [C,C_vec] = Cost(ClearedTrajectories, x, T_days);
+    
+end
---
+++ Parameters Calculation/plots.m
@@ -0,0 +1,57 @@
+close all;
+
+figure(1);
+%plot(x_rev(1,:))
+plot(cellXrev{500, 1}(1,:))
+hold on
+plot(ClearedTrajectories.Susceptible);
+title("Susceptible");
+
+figure(2);
+%plot(x_rev(2,:))
+plot(cellXrev{500, 1}(2,:))
+hold on
+plot(ClearedTrajectories.Infected);
+title("Unvaccinated Infected");
+
+figure(3);
+%plot(x_rev(3,:))
+plot(cellXrev{500, 1}(3,:))
+hold on
+plot(ClearedTrajectories.VaccinatedInfected);
+title("Vaccinated Infected");
+
+figure(4);
+%plot(x_rev(4,:))
+plot(cellXrev{500, 1}(4,:))
+hold on
+plot(ClearedTrajectories.Hospitalized);
+title("Unvaccinated Hospitalized (Acutely Symptomatic)");
+
+figure(5);
+%plot(x_rev(5,:))
+plot(cellXrev{500, 1}(5,:))
+hold on
+plot(ClearedTrajectories.Recovered);
+title("Recovered");
+
+figure(6);
+%plot(x_rev(6,:))
+plot(cellXrev{500, 1}(6,:))
+hold on
+plot(ClearedTrajectories.Extinct);
+title("Extinct");
+
+figure(7);
+%plot(x_rev(7,:))
+plot(cellXrev{500, 1}(7,:))
+hold on
+plot(ClearedTrajectories.VaccinatedSusceptible);
+title("Vaccinated Susceptible");
+
+figure(8);
+%plot(x_rev(8,:))
+plot(cellXrev{500, 1}(8,:))
+hold on
+plot(ClearedTrajectories.VaccinatedHospitalized);
+title("Vaccinated Hospitalized");
